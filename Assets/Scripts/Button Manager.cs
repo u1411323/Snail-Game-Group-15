d9ad0340a8074ae454787aa1747ceed3f78e6ac2
u1411323 <143031534+u1411323@users.noreply.github.com>
2024-09-21 13:38:26 -0600

    Added saving progress

--- Assets/Scripts/Button Manager.cs
+++ Assets/Scripts/Button Manager.cs
@@ -9,4 +9,9 @@ public class ButtonManager : MonoBehaviour
     {
         SceneManager.LoadScene("Game");
     }
+
+    public void GivePlayerLoreMenu()
+    {
+        Debug.Log("LORE");
+    }
 }
---
+++ Assets/Scripts/SaveData/SaveData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SaveDataScriptableObject", menuName = "ScriptableObjects/SaveData")] 
+public class SaveData : ScriptableObject
+{
+    public int pagesCollected = 0;
+}
