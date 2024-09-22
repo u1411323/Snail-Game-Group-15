8cde4208194c03ee7ac48d49caa09cfb4f1261d7
projectmaker1234 <andyliu09871@gmail.com>
2024-09-21 19:09:12 -0600

    Distance scaling

--- Assets/Scripts/DistanceManager.cs
+++ Assets/Scripts/DistanceManager.cs
@@ -6,26 +6,55 @@ using UnityEngine;
 public class DistanceManager : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI distanceText;
+    [SerializeField] private float[] scaling;
+    [SerializeField] private int[] milestones;
     private int distance;
     private float second;
-
+    private int milestone;
+    private int milestoneIndex;
+    public int scalingIndex;
+    private bool scaleChanged;
+    private bool milestoneChanged;
     // Start is called before the first frame update
     void Start()
     {
         distance = 0;
+        scalingIndex = 0;
+        milestoneIndex = 0;
+        milestone = milestones[milestoneIndex];
+        scaleChanged = false;
+        milestoneChanged = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         second += Time.deltaTime;
-        if (second >= .1)
+        if (second >= scaling[scalingIndex])
         {
             second = 0;
-            distance += 10;
+            distance += 50;
         }
-        
-        Debug.Log(distance);
+
+        if (distance == milestone)
+        {
+            if (milestoneIndex < milestones.Length - 1 && !milestoneChanged)
+            {
+                milestoneChanged = true;
+                milestoneIndex += 1;
+                milestone = milestones[milestoneIndex];
+            }
+
+            if (scalingIndex < scaling.Length - 1 && !scaleChanged)
+            {
+                scaleChanged = true;
+                scalingIndex += 1;
+            }
+
+            milestoneChanged = false;
+            scaleChanged = false;
+        }
+
         distanceText.text = "DISTANCE TRAVELED: " + distance.ToString() +" M";
     }
 }
--- Assets/Scripts/MovingLeft.cs
+++ Assets/Scripts/MovingLeft.cs
@@ -5,20 +5,26 @@ using UnityEngine;
 public class MovingLeft : MonoBehaviour
 {
     [SerializeField] float maxLeft = 0;
-    [SerializeField] float speed = 1;
-
+    [SerializeField] private float[] speed;
+    private float speedValue;
+    private DistanceManager dm;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        GameObject obj = GameObject.Find("Player");
+        dm = obj.GetComponent<DistanceManager>();
+        speedValue = speed[dm.scalingIndex];
     }
 
     // Update is called once per frame
     void Update()
     {
         if (this.transform.position.x > maxLeft)
-            this.transform.position = new Vector2(this.transform.position.x - speed * Time.deltaTime, this.transform.position.y);
+        {
+            this.transform.position = new Vector2(this.transform.position.x - speedValue * Time.deltaTime, this.transform.position.y);
+            Debug.Log(speedValue);
+        }
         else
             Destroy(this.gameObject);
     }
